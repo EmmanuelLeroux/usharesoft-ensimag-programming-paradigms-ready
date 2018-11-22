7a0c0e2154e00b8798dbb0befd4c8160a326a4d6
Emmanuel Leroux <emmanuel.leroux@ensimag.grenoble-inp.fr>
2018-11-22 19:32:33 +0100

    Procedural solution and Functional solution

--- functional-solution.py
+++ functional-solution.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+
+import json
+
+
+def sort(list1, key):
+    list2 = list1[:]
+    list2.sort(key=key)
+    return list2
+
+
+with open('beer_list.json') as file:
+    beer_list = json.load(file)
+
+sorted_beer_list = sort(beer_list, lambda elem: elem['Tx_Alcool'] if elem['Tx_Alcool'] else -1)
+
+for beer in sorted_beer_list:
+    print(beer['Nom'], beer['Tx_Alcool'])
--- procedural-solution.py
+++ procedural-solution.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+
+import json
+
+with open('beer_list.json') as file:
+    beer_list = json.load(file)
+
+beer_list.sort(key=lambda elem: elem['Tx_Alcool'] if elem['Tx_Alcool'] else -1)
+
+# for i in range(len(beer_list)):
+#     min = beer_list[i]["Tx_Alcool"]
+#     index = i
+#     for j in range(i+1, len(beer_list)):
+#         if beer_list[j]["Tx_Alcool"] is None:
+#             beer_list[j]["Tx_Alcool"] = -1
+#         if min > beer_list[j]["Tx_Alcool"]:
+#             min = beer_list[j]["Tx_Alcool"]
+#             index = j
+#     beer_list[i], beer_list[index] = beer_list[index], beer_list[i]
+
+for beer in beer_list:
+    print(beer['Nom'], beer['Tx_Alcool'])
